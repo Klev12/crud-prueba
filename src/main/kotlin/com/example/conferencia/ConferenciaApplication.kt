01c19d7e0e5d2434ac93b811865529547fba902a
Klev12 <kevinsibri2003@hotmail.com>
2023-11-06 09:00:05 -0500

    first commit

---
+++ src/main/kotlin/com/example/conferencia/ConferenciaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.conferencia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConferenciaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConferenciaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/conferencia/ConferenciaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.conferencia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConferenciaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
